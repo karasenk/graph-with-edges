67bb34982bcc115ecc78d155d3b7fb9a06a5cd6f
Nadia <90277588+karasenk@users.noreply.github.com>
2022-03-01 16:54:07 +0300

    Update way.py
    (оно правда теперь не правильно показывает, но я исправлю)

--- way.py
+++ way.py
@@ -1,10 +1,14 @@
+import math
+
+
 class Board:
     def __init__(self, edges, start, finish):
         self.edges = edges
         self.start = start
         self.finish = finish
-        self.board = [[-1 for _ in range(5)] for __ in range(7)]
+        self.board = [[math.inf for _ in range(5)] for __ in range(7)]
         self.visited = []
+        self.graph = [[math.inf for _ in range(5)] for __ in range(7)]  # для каждой вершины из какой вершины в неё пришли
 
     def get_value(self, coord0, coord1):
         for e in self.edges:
@@ -17,12 +21,13 @@ class Board:
         for coord in coords:
             x, y = coord
             weight = self.get_value(coord, coord0)
-            if self.board[x][y] > n + weight or \
-               self.board[x][y] == -1:
+            if self.board[x][y] > n + weight:
                 self.board[x][y] = n + weight
+                self.graph[x][y] = coord0
 
-    def walk(self, x, y, n):
+    def walk(self, coord0, x, y, n):
         self.board[x][y] = n
+        self.graph[x][y] = coord0
         self.visited.append((x, y))
         v = []
         if x - 1 >= 0 and (x - 1, y) not in self.visited:
@@ -37,37 +42,22 @@ class Board:
             self.mark(v, n, (x, y))
             next_point = min(v, key=lambda a: self.board[a[0]][a[1]])
             n += self.get_value((x, y), next_point)
-            return self.walk(next_point[0], next_point[1], n)
+            return self.walk((x, y), next_point[0], next_point[1], n)
         return True
 
-    def get_way(self, x, y, way, n):
-        n += self.board[x][y]
-        way.append((x, y))
-        if (x, y) == self.finish:
-            return way, n
-        ways = []
-        if x - 1 >= 0 and (x - 1, y) not in way:
-            w0 = [q for q in way]  # если передать way вместо w0 то оно почему то все пути в один список положит
-            ways.append(self.get_way(x - 1, y, w0, n))
-        if x + 1 < 7 and (x + 1, y) not in way:
-            w1 = [q for q in way]
-            ways.append(self.get_way(x + 1, y, w1, n))
-        if y - 1 >= 0 and (x, y - 1) not in way:
-            w2 = [q for q in way]
-            ways.append(self.get_way(x, y - 1, w2, n))
-        if y + 1 < 5 and (x, y + 1) not in way:
-            w3 = [q for q in way]
-            ways.append(self.get_way(x, y + 1, w3, n))
-        ways = list(filter(lambda z: len(z) > 0, ways))
-        if ways:
-            return min(ways, key=lambda w: w[1])
-        return []
+    def get_way(self, x, y, way):
+        if self.start == (x, y) or not isinstance(self.graph[x][y], tuple):
+            return way
+        way.append(self.graph[x][y])
+        print(self.graph[x][y])
+        x1, y1 = self.graph[x][y]
+        return self.get_way(x1, y1, way)
 
 
     # это - функция, которую, получается, нужно импортировать в код с движением.
     # принимает список рёбер с их весом, координату старта, координату финиша.
     # принимает координаты у которых отсчёт начинается с 1 (н-р не (0, 0), а (1, 1))
-    # возвращает путь в виде списка координат (н-р [(1, 2), (1, 3), (1, 4)]) (отсчёт тоже с 1)
+    # возвращает путь в виде списка координат (н-р [(1, 2), (1,   3), (1, 4)]) (отсчёт тоже с 1)
 def shortest_way(edges0, start, finish):
     start = start[0] - 1, start[1] - 1
     finish = finish[0] - 1, finish[1] - 1
@@ -78,6 +68,7 @@ def shortest_way(edges0, start, finish):
         edges.append([a, b, int(coord[2])])
     board = Board(edges, start, finish)
     x, y = board.start
-    board.walk(x, y, 0)
-    way = board.get_way(x, y, [], 0)[0]
+    board.walk((x, y), x, y, 0)
+    x1, y1 = board.finish
+    way = board.get_way(x1, y1, [])
     return [(coord[0] + 1, coord[1] + 1) for coord in way]
